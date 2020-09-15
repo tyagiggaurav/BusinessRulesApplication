550d49c51e797cb00c429079df1cb957cd428207
Gaurav Tyagi <tyagiggaurav@gmail.com>
2020-09-15 16:13:18 +0530

    Added unit test project.

---
+++ BusinessRulesUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BusinessRulesUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Harman Corporate")]
+[assembly: AssemblyProduct("BusinessRulesUnitTest")]
+[assembly: AssemblyCopyright("Copyright © Harman Corporate 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("97cc0b46-eddf-43a8-8fcc-04ad0185e4d2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BusinessRulesUnitTest/RulesTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessRulesUnitTest
+{
+    [TestClass]
+    public class RulesTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
